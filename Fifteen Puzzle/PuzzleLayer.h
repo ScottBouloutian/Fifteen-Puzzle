fdb833cd8eda30594cdef45d8171645df0d26de7
ScottBouloutian <ScottBouloutian@gmail.com>
2013-03-10 22:57:34 -0400

    Switched UI design from manual coding to Cocos Builder

---
+++ Fifteen Puzzle/PuzzleLayer.h
@@ -0,0 +1,16 @@
+//
+//  PuzzleLayer.h
+//  Fifteen Puzzle
+//
+//  Created by Scott Bouloutian on 3/10/13.
+//  Copyright 2013 Scott Bouloutian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface PuzzleLayer : CCLayer {
+    
+}
+
+@end
---
+++ Fifteen Puzzle/PuzzleLayer.m
@@ -0,0 +1,16 @@
+//
+//  PuzzleLayer.m
+//  Fifteen Puzzle
+//
+//  Created by Scott Bouloutian on 3/10/13.
+//  Copyright 2013 Scott Bouloutian. All rights reserved.
+//
+
+#import "PuzzleLayer.h"
+
+
+@implementation PuzzleLayer
+-(void)scrambleTouched:(id)sender{
+    
+}
+@end
